41e18cb3ad62669c8b728adfadbd56f10c35ff05
JosueRauda7 <katerozarack@gmail.com>
2020-04-05 18:25:16 -0600

    Prueba en index video y audio

--- js/video.js
+++ js/video.js
@@ -1,4 +1,9 @@
 navigator.mediaDevices.getUserMedia({ audio: true, video: true }).then((stream) => {
     console.log(stream)
+
+    let video = document.getElementById('video')
+    video.srcObject = stream
+    video.onloadedmetadata = (ev) => video.play()
+
 }).catch((error) => console.log(error))
 
