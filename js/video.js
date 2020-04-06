36be382d901b194a94c858986da8e8dffbb3d423
Josué Milton Rauda Ramírez <30637512+JosueRauda7@users.noreply.github.com>
2020-04-05 18:42:29 -0600

    Delete video.js

--- js/video.js
+++
@@ -1,9 +0,0 @@
-navigator.mediaDevices.getUserMedia({ audio: true, video: true }).then((stream) => {
-    console.log(stream)
-
-    let video = document.getElementById('video')
-    video.srcObject = stream
-    video.onloadedmetadata = (ev) => video.play()
-
-}).catch((error) => console.log(error))
-
