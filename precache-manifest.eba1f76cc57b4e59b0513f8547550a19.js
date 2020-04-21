55a9246b6d9a55e71deb3168ee460d357950a6e1
JosueRauda7 <katerozarack@gmail.com>
2020-04-20 22:11:30 -0600

    Updates

---
+++ precache-manifest.eba1f76cc57b4e59b0513f8547550a19.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cafec6f8364428ea26dcba9af01f4607",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7087c10d51034a02d3b8",
+    "url": "/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "a5aba7bd7430062b7e8a",
+    "url": "/static/css/main.4b5b1b7d.chunk.css"
+  },
+  {
+    "revision": "7087c10d51034a02d3b8",
+    "url": "/static/js/2.8cd47f82.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8cd47f82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5aba7bd7430062b7e8a",
+    "url": "/static/js/main.ad11fb86.chunk.js"
+  },
+  {
+    "revision": "5ede01677093a27a9a74",
+    "url": "/static/js/runtime-main.3a5a3f52.js"
+  }
+]);
